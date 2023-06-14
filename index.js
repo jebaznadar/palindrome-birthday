99a4076caeaed8c363f710af017933add841fdb5
jebaznadar <126007453+jebaznadar@users.noreply.github.com>
2023-06-14 20:30:39 +0530

    ex4

--- index.js
+++ index.js
@@ -30,6 +30,14 @@ function getDateAsString(date){
     return dateInStr ; 
 
 }
-var date = {day:'3', month:'2',year:'1999'}
-
-console.log(getDateAsString(date));
+function getDataInAllFormats(date){
+    var ddmmyyyy = date.day + date.month + date.year 
+    var mmddyyyy = date.month + date.day + date.year
+    var yyyymmdd = date.year + date.month + date.day
+    var ddmmyy = date.day + date.month + date.year.slice(-2)
+    var mmddyy = date.month + date.day + date.year.slice(-2)
+    var yyddmm  = date.year.slice(-2)  + date.day + date.month ;
+    return [ddmmyyyy,mmddyyyy, yyyymmdd ,ddmmyy,mmddyy,yyddmm]
+}
+var date = { day:'2' , month:'4', year:'1999'}
+console.log(getDataInAllFormats(getDateAsString(date)))
